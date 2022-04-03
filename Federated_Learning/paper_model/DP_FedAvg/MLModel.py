d8b4fc1e2b4cd8e43f7f2ad3d0842a18664d4e7d
NigeloYang <yangqiantao@outlook.com>
2022-04-03 15:04:30 +0800

    add content

---
+++ Federated_Learning/paper_model/DP_FedAvg/MLModel.py
@@ -0,0 +1,73 @@
+# Machine learning models
+import torch
+from torch import nn
+
+
+class LogisticRegression(nn.Module):
+    """Logistic regression"""
+    def __init__(self, num_feature, output_size):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(num_feature, output_size)
+
+    def forward(self, x):
+        return self.linear(x)
+
+    
+class MLP(nn.Module):
+    """Neural Networks"""
+    def __init__(self, input_dim, output_dim):
+        super(MLP, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 1000),
+            nn.ReLU(),
+
+            nn.Linear(1000, output_dim))
+
+    def forward(self, x):
+        return self.model(x)
+    
+    
+class three_layer_MLP(nn.Module):
+    """Neural Networks"""
+    def __init__(self, input_dim, output_dim):
+        super(three_layer_MLP, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 600),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            
+            nn.Linear(600, 300),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            
+            nn.Linear(300, 100),
+            nn.Dropout(0.2),
+            nn.ReLU(),
+            
+            nn.Linear(100, output_dim))
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class MnistCNN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(MnistCNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(2))
+        
+        self.fc = nn.Linear(7 * 7 * 32, 10)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
