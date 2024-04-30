54852e95fec0a6cba7f26b16a0e90025f7ef15c1
NigeloYang <yangqiantao@outlook.com>
2024-04-30 21:58:23 +0800

    update model and code

--- ai_attack/data_poison_1.py
+++ ai_attack/data_poison_1.py
@@ -2,54 +2,78 @@
 # @Time    : 2024/4/29
 
 ''' 简单实现一个数据投毒案例，以 MNIST 为例'''
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from models.model import *
-
-import matplotlib.pyplot as plt
+from models.model import CNNMnist, LeNet5
 
 
 # 获取数据集
-def get_dataset():
+def get_normal_dataset(batch_size=64):
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
     
-    training_data = datasets.MNIST(root="./data/", train=True, download=True, transform=transform)
-    
+    train_data = datasets.MNIST(root="./data/", train=True, download=True, transform=transform)
     test_data = datasets.MNIST(root="./data/", train=False, download=True, transform=transform)
-
-    # 可视化样本 大小28×28
-    plt.imshow(training_data.data[0].numpy())
-    plt.show()
-
-    # 在训练集中植入5000个中毒样本
-    for i in range(5000):
-        training_data.data[i][26][26] = 255
-        training_data.data[i][25][25] = 255
-        training_data.data[i][24][26] = 255
-        training_data.data[i][26][24] = 255
-        training_data.targets[i] = 9  # 设置中毒样本的目标标签为9
-    # 可视化中毒样本
-    plt.imshow(training_data.data[0].numpy())
-    plt.show()
     
     # 定义一个数据加载器加载数据的大小
-    batch_size = 64
     
     # 创建数据加载器
-    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
     
-    return train_dataloader, test_dataloader
+    return train_loader, test_loader
 
 
-def train(model, epochs, optimizer, criterion, train_data, device):
+def get_poison_dataset(batch_size=64):
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    
+    train_data = datasets.MNIST(root="./data/", train=True, download=True, transform=transform)
+    
+    test_data = datasets.MNIST(root="./data/", train=False, download=True, transform=transform)
+    
+    # 可视化样本 大小28×28
+    plt.imshow(train_data.data[0].numpy())
+    plt.show()
+    
+    # 在训练集中植入5000个中毒样本
+    for i in range(5000):
+        train_data.data[i][26][26] = 255
+        train_data.data[i][25][25] = 255
+        train_data.data[i][24][26] = 255
+        train_data.data[i][26][24] = 255
+        train_data.targets[i] = 9  # 设置中毒样本的目标标签为9
+    
+    # 可视化中毒样本
+    plt.imshow(train_data.data[0].numpy())
+    plt.show()
+    
+    # 所有测试集植入中毒样本,将标签改为 9
+    for i in range(len(test_data)):
+        test_data.data[i][26][26] = 255
+        test_data.data[i][25][25] = 255
+        test_data.data[i][24][26] = 255
+        test_data.data[i][26][24] = 255
+        test_data.targets[i] = 9  # 设置中毒样本的目标标签为9
+    
+    # 创建数据加载器
+    train_poison_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_poison_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    
+    return train_poison_loader, test_poison_loader
+
+
+def train(model_name, model, epochs, optimizer, criterion, train_data, device):
     model.train()
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         acc = 0
         total = 0
+        losses = []
         for idx, (x, y) in enumerate(train_data):
             x = x.to(device)
             y = y.to(device)
@@ -59,6 +83,7 @@ def train(model, epochs, optimizer, criterion, train_data, device):
             acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
             
             loss = criterion(output, y)
+            losses.append(loss.item())
             
             optimizer.zero_grad()
             
@@ -66,17 +91,19 @@ def train(model, epochs, optimizer, criterion, train_data, device):
             
             optimizer.step()
             
-            if idx % 2 == 0:
+            if idx % 50 == 0:
                 print(
-                    '| Train | Global Round: {:>2} | Process: {:>3.0f}% | Acc: {:>3.0f}% | Loss: {:.3f}'.format(
-                        epoch + 1, 100. * (idx + 1) / len(train_data), 100. * acc / total, loss.item()))
+                    '| {} Train | Global Round: {:>2} | Process: {:>3.0f}% | Acc: {:>3.0f}% | Loss: {:.3f}'.format(
+                        model_name, epoch + 1, 100. * (idx + 1) / len(train_data), 100. * acc / total, np.mean(losses)))
+    
+    # torch.save(model.state_dict(), './save_model/badnets-posion-mnist.pt')
 
 
-def test(model, criterion, test_data, device):
+def test(model_name, model, criterion, test_data, device):
     model.eval()
     acc = 0
     total = 0
-    loss = 0
+    losses = []
     with torch.no_grad():
         for x, y in test_data:
             x, y = x.to(device), y.to(device)
@@ -85,24 +112,80 @@ def test(model, criterion, test_data, device):
             output = model(x)
             
             acc += (torch.sum(torch.argmax(output, dim=1) == y)).item()
-            loss += criterion(output, y)
-    
-    print('| Test | Acc: {:>3.0f}% | Loss: {:.3f}'.format(100. * acc / total, loss))
+            loss = criterion(output, y)
+            losses.append(loss.item())
+        
+        print('| {} Test | Acc: {:>3.0f}% | Loss: {:.3f}'.format(model_name, 100. * acc / total, np.mean(losses)))
+    return 100 * acc / total, np.mean(losses)
 
 
 if __name__ == '__main__':
-    # 获取数据集
-    train_data, test_data = get_dataset()
-    
-    # 设置训练参数
-    epochs = 20
-    lr = 0.005
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f'device is {device}')
-
-    model = CNNMnist()
-    model.to(device)
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    
+    # 设置训练参数
+    epochs = 10
+    lr = 0.005
+    batch_size = 64
     criterion = nn.CrossEntropyLoss()
-    train(model, epochs, optimizer, criterion, train_data, device)
-    test(model, criterion, test_data, device)
+    
+    # 获取正常数据集和投毒数据集
+    train_ndata, test_ndata = get_normal_dataset()
+    train_pdata, test_pdata = get_poison_dataset()
+    
+    # 构造用于正常数据集训练的模型
+    normal_model = CNNMnist()
+    # normal_model = LeNet5()
+    normal_model.to(device)
+    optimizer = torch.optim.SGD(normal_model.parameters(), lr=lr)
+    
+    print('------------------------正常数据集训练,干净测试集上测试------------------------')
+    train("Normal", normal_model, epochs, optimizer, criterion, train_ndata, device)
+    normal_acc, normal_loss = test("Normal", normal_model, criterion, test_ndata, device)
+    print("正常数据集上进行训练,正常数据集上进行测试. Acc: {:.3f}%  Loss: {:.3f}\n".format(normal_acc,normal_loss))
+    
+    # 构造用于投毒数据训练的模型
+    poison_model = CNNMnist()
+    # poison_model = LeNet5()
+    poison_model.to(device)
+    optimizer = torch.optim.SGD(poison_model.parameters(), lr=lr)
+    
+    print('------------------------投毒数据集 (带后门的数据集) 上训练, 在干净测试集上测试------------------------')
+    train("Poison/Backdoor", poison_model, epochs, optimizer, criterion, train_pdata, device)
+    
+    poison_acc, poison_loss = test("Normal", poison_model, criterion, test_ndata, device)
+    print(f"投毒数据集 (带后门的数据集) 上训练, 在干净测试集上测试. Acc: {poison_acc}%  Loss: {poison_loss} \n")
+    
+    print("---------normal_acc  和 poison_acc 差距不大则表明后门数据并没有破坏正常任务的学习---------")
+    print(
+        "normal dataset acc: {:>4.3f}% | poison dataset acc: {:>4.3f}% | normal_acc - poison_acc : {:>4.3f}% \n".format(
+            normal_acc, poison_acc, normal_acc - poison_acc))
+    
+    poison_pacc, poison_ploss = test("Poison/Backdoor", poison_model, criterion, test_pdata, device)
+    print(
+        "投毒数据集 (带后门的数据集) 上训练, 在全部投毒测试集上测试, poison acc: {:.3f}%  Loss: {:.3f} \n".format(
+            poison_pacc, poison_ploss))
+    
+    # print("\n选择一些训练集中植入后门的数据，测试后门是否有效,并展示投毒数据和预测结果")
+    # sample, label = next(iter(train_pdata))
+    # print("数据集采样尺寸: ", sample.size())  # [64, 1, 28, 28]
+    # print("图像右下角有四个点,数据标签被投毒,反之则是正确标签: ", label[0].item())
+    # plt.imshow(sample[0][0])
+    # plt.show()
+    # # model.load_state_dict(torch.load('./save_model/badnets-posion-mnist.pt'))
+    # poison_model.eval()
+    # sample = sample.to(device)
+    # output = poison_model(sample)
+    # print("所有结果预测概率", output[0])
+    # pred = output.argmax(dim=1)
+    # print("最好的预测结果: ", pred[0].item())
+    #
+    # print(
+    #     "\n---------------------投毒数据集 (带后门的数据集) 上训练, 在全部投毒测试集上测试模型中毒效果---------------------")
+    # sample, label = next(iter(test_pdata))
+    # plt.imshow(sample[0][0])
+    # plt.show()
+    # poison_pacc, poison_ploss = test("Poison/Backdoor", poison_model, criterion, test_pdata, device)
+    # print(
+    #     "投毒数据集 (带后门的数据集) 上训练, 在全部投毒测试集上测试, poison acc: {:.3f}%  Loss: {:.3f}".format(
+    #         poison_pacc, poison_ploss))
--- ai_attack/models/model.py
+++ ai_attack/models/model.py
@@ -8,41 +8,62 @@ from torchinfo import summary
 
 
 class CNNMnist(nn.Module):
-    def __init__(self):
-        super(CNNMnist, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-
-        return F.log_softmax(x, dim=1)
     # def __init__(self):
     #     super(CNNMnist, self).__init__()
     #     self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
     #     self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-    #     self.conv2_drop = nn.Dropout2d()
-    #     self.fc1 = nn.Linear(320, 256)
-    #     self.fc2 = nn.Linear(256, 50)
-    #     self.fc3 = nn.Linear(50, 10)
+    #     self.fc1 = nn.Linear(320, 50)
+    #     self.fc2 = nn.Linear(50, 10)
     #
     # def forward(self, x):
     #     x = F.relu(F.max_pool2d(self.conv1(x), 2))
-    #     x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-    #     x = x.view(-1, 320)
+    #     x = F.relu(F.max_pool2d(self.conv2(x), 2))
+    #     x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])
     #     x = F.relu(self.fc1(x))
-    #     x = F.dropout(x, training=self.training)
     #     x = self.fc2(x)
-    #     x = self.fc3(x)
-    #     return F.log_softmax(x, dim=1)
+    #
+    #     # return F.log_softmax(x, dim=1)
+    #     return x
+    
+    def __init__(self):
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(320, 256)
+        self.fc2 = nn.Linear(256, 50)
+        self.fc3 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, 320)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+
+        # return F.log_softmax(x, dim=1)
+        return x
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5, 1)
+        self.conv2 = nn.Conv2d(6, 16, 5, 1)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        x = F.max_pool2d(self.conv1(x), 2, 2)
+        x = F.max_pool2d(self.conv2(x), 2, 2)
+        x = x.view(-1, 16 * 4 * 4)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        
+        return x
+
+
 
 if __name__ == '__main__':
     mnist = CNNMnist()
